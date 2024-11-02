61606431454f8a80ef1e20d1b35d7d54c8d404b2
marbenz2 <benzinger.marcel@gmail.com>
2024-11-02 13:58:04 +0100

    fix: added supressHydrationWarning due to error messages with browser addons

--- components/ui/searchbar.tsx
+++ components/ui/searchbar.tsx
@@ -7,7 +7,12 @@ import { Input } from "./input";
 export default function Searchbar() {
   return (
     <div className="relative flex w-full py-4">
-      <Input type="search" placeholder="Suchen..." className="w-full" />
+      <Input
+        type="search"
+        placeholder="Suchen..."
+        className="w-full"
+        suppressHydrationWarning
+      />
       <Button className="absolute right-0 rounded-l-none">
         <SearchIcon />
       </Button>
