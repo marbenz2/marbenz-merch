634d4745db355bc5593361b9d23f57e6cb417ba5
marbenz2 <benzinger.marcel@gmail.com>
2024-11-06 13:31:03 +0100

    style: slightly changed gaps and padding

--- components/info/InfoBar.tsx
+++ components/info/InfoBar.tsx
@@ -8,8 +8,8 @@ import React from "react";
 
 export default function InfoBar() {
   return (
-    <div className="flex w-full py-1 sm:py-2 bg-contrast mt-2 items-center px-6 lg:px-12">
-      <div className="flex w-full max-w-7xl mx-auto justify-between gap-4 text-muted font-semibold text-xs lg:text-sm">
+    <div className="flex w-full py-1 sm:py-2 bg-contrast mt-2 items-center px-1 md:px-6 lg:px-12">
+      <div className="flex w-full max-w-7xl mx-auto justify-between gap-2 text-muted font-semibold text-xs lg:text-sm">
         <p className="flex items-center gap-2">
           <PackageIcon className="w-6 h-6 shrink-0" />
           <span>Lieferung innerhalb 1-3 Tage</span>
