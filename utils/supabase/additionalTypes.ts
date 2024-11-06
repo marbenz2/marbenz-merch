49c2aba12edcedffcd0f164b15a5cff506246c3c
marbenz2 <benzinger.marcel@gmail.com>
2024-11-06 12:26:47 +0100

    refactor: updated the additional types

--- utils/supabase/additionalTypes.ts
+++ utils/supabase/additionalTypes.ts
@@ -1,5 +1,17 @@
 import { Tables } from "@/database.types";
 
-export type ProductWithImage = Tables<"products"> & {
+export type Categories = Tables<"categories">;
+export type Product = Tables<"products">;
+
+export type Products = Tables<"products"> & {
   primaryImage?: string;
+  categories?: Tables<"categories">[];
+};
+
+export type ProductWithCategories = Product & { categories?: Categories[] };
+
+export type OrderWithItems = Tables<"orders"> & {
+  order_items: (Tables<"order_items"> & {
+    products: Tables<"products"> & { primaryImage?: string };
+  })[];
 };
