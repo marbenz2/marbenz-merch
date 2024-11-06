566e394cad0a760bd101207fbe046f9acebbdf4c
marbenz2 <benzinger.marcel@gmail.com>
2024-11-06 13:18:56 +0100

    refactor: reworked mobile view

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,17 +5,19 @@ import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full px-12 py-4 bg-background-navigation">
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="flex flex-col gap-4 w-full h-full">
+    <footer className="flex w-full px-4 lg:px-12 py-6 bg-background-navigation border-t-4 border-border-navigation">
+      <div className="flex flex-col md:flex-row w-full gap-12">
+        <div className="flex flex-col basis-1/3 gap-4 w-full h-full">
           <Logo />
           <Location />
         </div>
-        <div className="flex items-end justify-center w-full h-full">
-          <Copyright />
-        </div>
-        <div className="flex items-end justify-start w-full h-full">
-          <FooterLinks />
+        <div className="flex flex-col-reverse md:flex-row basis-2/3 w-full gap-8">
+          <div className="flex items-end justify-center md:justify-center w-full h-full">
+            <Copyright />
+          </div>
+          <div className="flex items-end justify-start md:justify-center w-full h-full">
+            <FooterLinks />
+          </div>
         </div>
       </div>
     </footer>
